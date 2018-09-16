768af85dae3af01127eff1913ff6a27f3d4fc312
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2018-09-17 06:37:02 +0900

    loginをemailからidに変更

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,15 +11,15 @@
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                        <div class="form-group{{ $errors->has('login_id') ? ' has-error' : '' }}">
+                            <label for="login_id" class="col-md-4 control-label">Login ID</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="login_id" type="login_id" class="form-control" name="login_id" value="{{ old('login_id') }}" required autofocus>
 
-                                @if ($errors->has('email'))
+                                @if ($errors->has('login_id'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        <strong>{{ $errors->first('login_id') }}</strong>
                                     </span>
                                 @endif
                             </div>
