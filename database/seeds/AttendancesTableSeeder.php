a572045277f5482f3a68ac8c51fb294120a42e5f
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-02-06 14:28:59 +0900

    attendancesのシーダーも対応

--- database/seeds/AttendancesTableSeeder.php
+++ database/seeds/AttendancesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class AttendancesTableSeeder extends Seeder
 {
@@ -14,14 +15,20 @@ class AttendancesTableSeeder extends Seeder
       // 一旦中身を削除する
       DB::table('attendances')->delete();
 
+      // 日付の取得
+      $year = date('Y');
+      $month = date('m');
+
+      list ($prev_year, $prev_month) = $this->get_prev_month($year, $month);
+
       DB::table('attendances')->insert([
         'name' => 'たろー',
         'part_id' => 2,
         'activity_id' => 7,
         'attendance' => 3,
         'comment' => "短いコメント",
-        'created_at' => '2018-09-10 18:00:00',
-        'updated_at' => '2018-09-10 18:10:00'
+        'created_at' => Carbon::create($prev_year, $prev_month, 20, 18, 0, 0),
+        'updated_at' => Carbon::create($prev_year, $prev_month, 20, 18, 10, 0)
       ]);
 
       DB::table('attendances')->insert([
@@ -30,8 +37,8 @@ class AttendancesTableSeeder extends Seeder
         'activity_id' => 8,
         'attendance' => 1,
         'comment' => "長いコメントは途中まで表示されます．長いコメントは途中まで表示されます．",
-        'created_at' => '2018-09-20 18:00:00',
-        'updated_at' => '2018-09-20 18:10:00'
+        'created_at' => Carbon::create($prev_year, $prev_month, 20, 18, 0, 0),
+        'updated_at' => Carbon::create($prev_year, $prev_month, 20, 18, 10, 0)
       ]);
 
       DB::table('attendances')->insert([
@@ -40,8 +47,8 @@ class AttendancesTableSeeder extends Seeder
         'activity_id' => 9,
         'attendance' => 1,
         'comment' => "あいうえお",
-        'created_at' => '2018-09-20 18:00:00',
-        'updated_at' => '2018-09-20 18:00:00'
+        'created_at' => Carbon::create($prev_year, $prev_month, 20, 18, 0, 0),
+        'updated_at' => Carbon::create($prev_year, $prev_month, 20, 18, 0, 0)
       ]);
 
       DB::table('attendances')->insert([
@@ -51,8 +58,8 @@ class AttendancesTableSeeder extends Seeder
         'attendance' => 3,
         'comment' => "コメント",
         'comment' => "あいうえおか",
-        'created_at' => '2018-09-20 18:00:00',
-        'updated_at' => '2018-09-20 18:00:00'
+        'created_at' => Carbon::create($prev_year, $prev_month, 20, 18, 0, 0),
+        'updated_at' => Carbon::create($prev_year, $prev_month, 20, 18, 0, 0)
       ]);
 
       DB::table('attendances')->insert([
@@ -61,8 +68,8 @@ class AttendancesTableSeeder extends Seeder
         'activity_id' => 7,
         'attendance' => 3,
         'comment' => "あいうえおかき",
-        'created_at' => '2018-08-11 18:00:00',
-        'updated_at' => '2018-08-11 18:00:03'
+        'created_at' => Carbon::create($prev_year, $prev_month, 11, 18, 0, 0),
+        'updated_at' => Carbon::create($prev_year, $prev_month, 11, 18, 0, 0)
       ]);
 
       DB::table('attendances')->insert([
@@ -71,8 +78,8 @@ class AttendancesTableSeeder extends Seeder
         'activity_id' => 8,
         'attendance' => 3,
         'comment' => "あいうえおかきく",
-        'created_at' => '2018-08-11 18:00:00',
-        'updated_at' => '2018-08-11 18:00:00'
+        'created_at' => Carbon::create($prev_year, $prev_month, 11, 18, 0, 0),
+        'updated_at' => Carbon::create($prev_year, $prev_month, 11, 18, 0, 0)
       ]);
 
       DB::table('attendances')->insert([
@@ -81,8 +88,8 @@ class AttendancesTableSeeder extends Seeder
         'activity_id' => 9,
         'attendance' => 3,
         'comment' => "これもコメント",
-        'created_at' => '2018-08-11 18:00:00',
-        'updated_at' => '2018-08-11 18:00:00'
+        'created_at' => Carbon::create($prev_year, $prev_month, 11, 18, 0, 0),
+        'updated_at' => Carbon::create($prev_year, $prev_month, 11, 18, 0, 0)
       ]);
 
       DB::table('attendances')->insert([
@@ -90,9 +97,30 @@ class AttendancesTableSeeder extends Seeder
         'part_id' => 3,
         'activity_id' => 10,
         'attendance' => 1,
-        'created_at' => '2018-08-11 18:00:00',
-        'updated_at' => '2018-08-11 18:00:00'
+        'created_at' => Carbon::create($prev_year, $prev_month, 11, 18, 0, 0),
+        'updated_at' => Carbon::create($prev_year, $prev_month, 11, 18, 0, 0)
       ]);
 
+    }
+
+    /**
+    *     前の月を取得する
+    *
+    *
+    **/
+    private function get_prev_month($year, $month)
+    {
+      $n_year = (int)$year;
+      $n_month = (int)$month;
+
+      if ($n_month == 1) {
+        $n_year--;
+        $n_month = 12;
+      } else {
+        $n_month--;
+      }
+      $year = sprintf("%04d", $n_year);
+      $month = sprintf("%02d", $n_month);
+      return array($year, $month);
     }
 }
