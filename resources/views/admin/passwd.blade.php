6eec5497dbe471d8ff0a2bf5cf66b710a3201e18
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-09-07 17:42:01 +0900

    view->old

--- resources/views/admin/passwd.blade.php
+++ resources/views/admin/passwd.blade.php
@@ -26,7 +26,7 @@
                         <input type="hidden" name="id" value="{{ $id }}">
 
                           <div class="form-group row">
-                              <label for="current_password" class="col-md-4 col-form-label text-md-right">{{ __('現在のパスワード') }}</label>
+                              <label for="current_password" class="col-md-4 col-form-label text-md-right">現在のパスワード</label>
 
                               <div class="col-md-6">
                                   <input id="current_password" type="password" class="form-control" name="current_password" value="{{ $current_password ?? old('current_password') }}" required autofocus>
@@ -40,7 +40,7 @@
                           </div>
 
                           <div class="form-group row">
-                              <label for="new_password" class="col-md-4 col-form-label text-md-right">{{ __('新しいパスワード') }}</label>
+                              <label for="new_password" class="col-md-4 col-form-label text-md-right">新しいパスワード</label>
 
                               <div class="col-md-6">
                                   <input id="new_password" type="password" class="form-control" name="new_password" required>
@@ -54,7 +54,7 @@
                           </div>
 
                           <div class="form-group row">
-                              <label for="new_password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('新しいパスワード（確認用として同じものを入力して下さい）') }}</label>
+                              <label for="new_password-confirm" class="col-md-4 col-form-label text-md-right">新しいパスワード（確認用として同じものを入力して下さい）</label>
 
                               <div class="col-md-6">
                                   <input id="new_password-confirm" type="password" class="form-control" name="new_password_confirmation" required>
@@ -65,7 +65,7 @@
                       <div class="form-group row mb-0">
                           <div class="col-md-6 offset-md-4">
                               <button type="submit" class="btn btn-primary btn-block">
-                                  {{ __('管理者パスワードを変更する') }}
+                                  管理者パスワードを変更する
                               </button>
                           </div>
                       </div>
