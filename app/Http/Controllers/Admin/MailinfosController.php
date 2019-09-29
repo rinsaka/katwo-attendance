6eac7861678038e7046037c8971596ab3947f163
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-09-29 22:35:14 +0900

    mailinfoのsave

--- app/Http/Controllers/Admin/MailinfosController.php
+++ app/Http/Controllers/Admin/MailinfosController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Mailinfo;
 
 class MailinfosController extends Controller
 {
   public function edit()
   {
-    dd('edit');
+    $mailinfo = Mailinfo::where('key', '=', 'signiture')
+                        ->first();
+    if (!$mailinfo) {
+      $mailinfo = new Mailinfo();
+      $mailinfo->key = 'signiture';
+      $mailinfo->mailinfo = 'メールフッタをここに';
+      $mailinfo->save();
+    }
+    dd('edit', $mailinfo);
   }
 
   public function update(Request $request)
