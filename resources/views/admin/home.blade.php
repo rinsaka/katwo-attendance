c871bffa07b1a08f828c0f30c3c1f8dcb77657f9
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2026-03-11 20:06:15 +0900

    admin home footer

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -48,68 +48,41 @@
   </div>
 </div>
 </main>
-<footer>
+<footer class="text-bg-info p-3">
 
-
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading"></div>
-
-
-                    {{-- <div>
-
-                        <p
-                          @if ($activity->meeting == 1)
-                           class="admin_meeting"
-                          @endif
-                        >
-                          @if ($activity->meeting == 1)
-                            【一部団員に限定】
-                          @endif
-
-
-                          </a>
-                        </p>
-                      @empty
-                      @endforelse
-
-                    </div> --}}
-
-
-                </div>
-
-                <div  class="panel-footer">
-                  <p>
-                  <a href="{{ action('Admin\HomeController@create') }}">
-                    活動予定を新規登録
-                  </a>
-                  </p>
-                  <p class='pull-right'>
-                    <a href="{{ action('Admin\MailinfosController@edit') }}">
-                      メールフッタを編集
-                    </a>
-                  </p>
-                  <p>&nbsp;</p>
-                  <p class='pull-right'>
-                    <a href="{{ action('Admin\HomeController@place') }}">
-                      活動施設の一覧表示
-                    </a>
-                  </p>
-                  <p>&nbsp;</p>
-                  <p class='pull-right'>
-                    <a href="{{ action('Admin\HomeController@time') }}">
-                      活動時間の一覧表示
-                    </a>
-                  </p>
-                  <p>&nbsp;</p>
-                  <p>
-                    This system is developed with <a href="https://laravel.com/" target="_blank">Laravel</a>, <a href="https://lolipop.jp/" target="_blank">LOLIPOP!</a> and <a href="https://github.com/rinsaka/katwo-attendance" target="_blank">GitHub</a>.
-                  </p>
-                </div>
-            </div>
-        </div>
-    </div>
+  <div class="">
+    <a href="{{ action('Admin\HomeController@create') }}">
+      活動予定を新規登録
+    </a>
   </div>
+
+  <div class="text-end">
+    <a href="{{ action('Admin\MailinfosController@edit') }}">
+      メールフッタを編集
+    </a>
+  </div>
+
+  <div class="text-end">
+    <a href="{{ action('Admin\HomeController@place') }}">
+      活動施設の一覧表示
+    </a>
+  </div>
+
+  <div class="text-end">
+    <a href="{{ action('Admin\HomeController@time') }}">
+      活動時間の一覧表示
+    </a>
+  </div>
+
+  <div class="">
+    This system is developed with <a href="https://laravel.com/" target="_blank">Laravel</a>, <a href="https://lolipop.jp/" target="_blank">LOLIPOP!</a> and <a href="https://github.com/rinsaka/katwo-attendance" target="_blank">GitHub</a>.
+  </div>
+
+  <div class="">
+    <a href="https://rinsaka.com/rinsaka/">
+      rinsaka.com
+    </a>
+  </div>
+
 </footer>
 @endsection
