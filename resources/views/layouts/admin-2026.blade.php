1bede5c8efddf7da918e1977ba66a5340118fc9e
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2026-03-11 20:18:07 +0900

    layout

--- resources/views/layouts/admin-2026.blade.php
+++ resources/views/layouts/admin-2026.blade.php
@@ -32,6 +32,9 @@
       </ul>
       @else
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="{{ action('Admin\HomeController@create') }}">活動予定を新規登録</a>
+        </li>
       </ul>
       <ul class="navbar-nav mb-2 mb-lg-0">
         <li class="nav-item dropdown">
